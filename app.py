ba20be95c98230f16429adb63d4e8ef55bd270ba
Dinesh babu <dinesh.babu890@gmail.com>
2018-05-25 16:18:47 +0530

    few changes and additions

--- app.py
+++ app.py
@@ -28,6 +28,22 @@ def login():
 def register():
 	return render_template('register.html')
 
+@app.route('/home')
+def home():
+   return render_template('home.html')
+
+@app.route('/page2')
+def page2():
+   return render_template('page2.html')
+
+@app.route('/page3')
+def page3():
+   return render_template('page3.html')
+
+@app.route('/page4')
+def page4():
+   return render_template('page4.html')
+
 @app.route('/logout')
 def logout():
    # remove the username from the session if it is there
